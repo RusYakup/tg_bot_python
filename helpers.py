e6bbc5924b08c5abee4d17d00e020104a92ac156
RusYakup <zregxbr@ya.ru>
2024-04-10 23:39:50 +0300

    fix: def wind

--- helpers.py
+++ helpers.py
@@ -1,38 +1,39 @@
-import datetime
+from pydantic import ValidationError
+from models import *
 
-def wind(key: str) -> str:
+вуа
+def wind(win_dir: str, wind_kph: float, max_wind_kph: float) -> str:
+    """
+        1) translates wind direction from English to Russian
+        2) converts wind speed km/h to m/s
+        Args:
+        win_dir (str): The direction of the wind in English.
+        wind_kph (float): The speed of the wind in kilometers per hour.
+        max_wind_kph (float): The maximum speed of the wind in kilometers per hour.
+        Returns:
+        str: Sentence indicating wind direction, m/s speed and maximum wind speed on that day
+        """
     direction = {
         'N': "Северный",
+        'NNE': "Северо-северо-восточный",
         'NE': "Северо восточный",
-        'E': "Восточный",
         'ENE': "Восточно-северо-восточный",
-        'S': "Южный",
-        'SW': "Юго западный",
-        'W': "Западный",
-        'NW': "Северо западный",
-        'SSE': "Юго-юго-восточный ",
+        'E': "Восточный",
+        'ESE': "Восточно-юго-восточный",
         'SE': "Северный юго-восточный",
-        'SSW': "Юго-юго-западный ",
+        'SSE': "Юго-юго-восточный",
+        'S': "Южный",
+        'SSW': "Юго-юго-западный",
         'SW': "Юго западный",
-        'WSW': "Западно-юго-западный ",
-        'WNW': "Западно-северо-западный ",
+        'WSW': "Западно-юго-западный",
+        'W': "Западный",
+        'WNW': "Западно-северо-западный",
         'NW': "Северный западный",
-        'NNW': "Северо-северо-западный "
+        'NNW': "Северо-северо-западный"
     }
-
-    if key in direction:
-        return direction[key]
+    wind_ms = round(wind_kph / 3.6)
+    max_wind_ms = round(max_wind_kph / 3.6)
+    if win_dir in direction:
+        return f"Ветер {direction[win_dir]} {wind_ms}м/с (с порывами до {max_wind_ms} м/с)"
     else:
         return "Направление ветра неизвестно"
-
-
-
-def get_today_date():
-    return datetime.date.today().strftime('%Y-%m-%d')
-
-today_date = get_today_date()
-
-
-
-
-
